1d43d18d2c121a8c9bb54e719497605d3220a596
ramon79 <47372895+ramon79@users.noreply.github.com>
2019-03-20 16:01:50 +0900

    done

---
+++ moving-objects/Assets/AccelerometerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccelerometerMove : MonoBehaviour
+{
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = Input.acceleration.x;
+        float z = Input.acceleration.z;
+        Debug.Log(temp);
+        transform.Translate(0, 0, - z * speed * Time.deltaTime);
+        transform.Rotate(0, 0, -temp * speed);
+        
+        
+
+        
+    }
+}
